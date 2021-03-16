4678dc7f83f5e6c92bb46dec8aea75a6c338ccd7
Aslam Sarfraz <aslam91r@gmail.com>
2021-03-16 20:01:10 +0500

    Add Payments Pages for Student and Teachers

---
+++ client/src/pages/payments/StudentPayments/StudentPaymentPage.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import MainMenu from '../../../components/MainMenu';
+import './style/studentpaymentpage.css';
+import { Row, Col } from 'react-bootstrap';
+
+
+function StudentPaymentPage() {
+    return (
+        <div>
+            <MainMenu />
+            <div className='SDP_MainSection'>
+                <h3>Student Dues</h3>
+                <div className='DuesSec'>
+                    <h4>2021</h4>
+                    <Row>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jan</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Feb</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Mar</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Apr</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>May</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jun</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jul</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Aug</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Sep</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Oct</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Nov</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Dec</div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className='PaymentSec'>
+                    <h4>2020</h4>
+                    <Row>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jan</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Feb</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Mar</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Apr</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>May</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jun</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jul</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Aug</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Sep</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Oct</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Nov</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Dec</div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className='PaymentSec'>
+                    <h4>2019</h4>
+                    <Row>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jan</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Feb</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Mar</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Apr</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>May</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jun</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jul</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Aug</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Sep</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Oct</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Nov</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Dec</div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className='PaymentSec'>
+                    <h4>2018</h4>
+                    <Row>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jan</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Feb</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Mar</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Apr</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>May</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jun</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jul</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Aug</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Sep</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Oct</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Nov</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Dec</div>
+                        </Col>
+                    </Row>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StudentPaymentPage;
---
+++ client/src/pages/payments/TeacherPayments/TeacherPaymentPage.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import MainMenu from '../../../components/MainMenu';
+import './style/teacherpaymentpage.css';
+import { Row, Col } from 'react-bootstrap';
+
+
+function TeacherPaymentPage() {
+    return (
+        <div>
+            <MainMenu />
+            <div className='TPP_MainSection'>
+                <h3>Teacher Payments</h3>
+                <div className='PaymentSec'>
+                    <h4>2021</h4>
+                    <Row>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jan</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Feb</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Mar</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Apr</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>May</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jun</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jul</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Aug</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Sep</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Oct</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Nov</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Dec</div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className='PaymentSec'>
+                    <h4>2020</h4>
+                    <Row>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jan</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Feb</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Mar</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Apr</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>May</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jun</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jul</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Aug</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Sep</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Oct</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Nov</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Dec</div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className='PaymentSec'>
+                    <h4>2019</h4>
+                    <Row>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jan</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Feb</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Mar</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Apr</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>May</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jun</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jul</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Aug</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Sep</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Oct</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Nov</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Dec</div>
+                        </Col>
+                    </Row>
+                </div>
+                <div className='PaymentSec'>
+                    <h4>2018</h4>
+                    <Row>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jan</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Feb</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Mar</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Apr</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>May</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jun</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Jul</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Aug</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Sep</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Oct</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Nov</div>
+                        </Col>
+                        <Col lg={1}>
+                            <div className='MonthIcon'>Dec</div>
+                        </Col>
+                    </Row>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TeacherPaymentPage;
